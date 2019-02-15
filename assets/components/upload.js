3a343e998c07911997e5698b6e133fc49f8eb4fe
Celyn Susanto <celyn.vy@gmail.com>
2019-02-15 14:06:26 +0700

    add

--- assets/components/upload.js
+++ assets/components/upload.js
@@ -31,7 +31,24 @@ Vue.component('upload-video', {
       .catch(err => {
         console.error(err)
       })
-    }, 
+    },
+    onSignIn(googleUser) {
+        var profile = googleUser.getBasicProfile();
+        console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+        console.log('Name: ' + profile.getName());
+        console.log('Image URL: ' + profile.getImageUrl());
+        console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+
+        var id_token = googleUser.getAuthResponse().id_token;
+
+        axios.post('http://localhost:3000/api/user/authentication/google', {id_token})
+            .then(({data}) => {
+                localStorage.setItem('token', `${data.token}`)
+            })
+            .catch(err => {
+                console.log(err)
+            })
+    },
     previewFiles() {
       this.files = this.$refs.myFiles.files
     },
