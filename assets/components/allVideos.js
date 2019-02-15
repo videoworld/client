4c391c47fd2b76fe25a41de295f5c108005977a9
Mahdihrs <mahdiharis@outlook.com>
2019-02-15 12:02:55 +0700

    add list video

--- assets/components/allVideos.js
+++ assets/components/allVideos.js
@@ -1,39 +1,43 @@
 Vue.component('all-videos', {
-    props: [''],
+    props: ['homepagevideos'],
+    methods: {
+        getTwitterText(title, url) {
+            let convert = url.split(' ').join('%20')
+            return `${title} ${convert}`
+        }
+    },
     template: `
-    <div id="video-detail" class="col-12 col-sm-6 col-md-8 scroll" >
-    <hr>
     <!--VIDEO-->
-    <div class="card z-depth-2 offset-md-1" style="width: 85%;">
-        <div class="view overlay">
-            <div class="card-body elegant-color white-text">
-                <a class="mr-4"><i class="fas fa-share-alt white-text"></i></a>
-                <h4 class="card-title">Video title</h4>
-                <hr class="hr-light">
-                <p class="card-text white-text mb-4">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-            </div>
-            <img class="card-img-bottom " src="https://mdbootstrap.com/img/Photos/Horizontal/Work/4-col/img%20%2821%29.jpg" alt="Card image cap">
-        </div>                            
-    </div>
+    <div id="video-detail" class="col-12 col-sm-6 col-md-8 scroll">
     <hr>
-    </div>
+        <div class="card z-depth-2 offset-md-1" style="width: 85%;" v-for="vid in homepagevideos">
+            <div class="view overlay">
+                <div class="card-body elegant-color white-text">
+                    <a class="mr-4"><i class="fas fa-share-alt white-text"></i></a>
+                    <h4 class="card-title">{{ vid.title }}</h4>
+                    <hr class="hr-light">
+                    <p class="card-text white-text mb-4">{{ vid.description }}</p>
+                    <a class="twitter-share-button"
+                        href="https://twitter.com/intent/tweet"
+                        data-size="large"
+                        :data-text="getTwitterText(vid.title, vid.videoUrl)"
+                        data-url="#VideoWorld"
+                        data-hashtags="VideoWorld"
+                        data-via="twitterdev"
+                        data-related="twitterapi,twitter">
+                    Tweet
+                    </a>
+                    <iframe src="https://www.facebook.com/plugins/share_button.php?href=https://storage.googleapis.com/miniwpstorage/1550204158199PMMB(PapaMarahMukaBopeng).mp4/&layout=button_count&size=large&mobile_iframe=true&width=83&height=28&appId" width="83" height="28" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true"></iframe>
+                    <div class="line-it-button" data-lang="en" data-type="share-d" data-ver="2" data-url="http://upvid.mahdihrs.world" style="display: none;"></div>
+                        <script src="https://d.line-scdn.net/r/web/social-plugin/js/thirdparty/loader.min.js" async="async" defer="defer"></script>
+                    </div>
+                    <a href="line://msg/text/<?php echo $post_permalink_encode; ?>" target="_blank">Share on LINE</a>
+                <div class="embed-responsive embed-responsive-16by9">
+                    <iframe class="embed-responsive-item" :src="vid.videoUrl" allowfullscreen></iframe>
+                </div>
+            </div>                            
+        </div>
+    </div>    
+    <hr>
     `
 })
-
-// <!--VIDEO-->
-// <!-- <div class="card z-depth-2 offset-md-1" style="width: 85%">
-//     <div class="view overlay">
-//         <div class="card-body elegant-color white-text rounded-bottom">
-//             <a class="activator waves-effect mr-4"><i class="fas fa-share-alt white-text"></i></a>
-//             <h4 class="card-title">Video title</h4>
-//             <hr class="hr-light">
-//             <p class="card-text white-text mb-4">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-//         </div>
-
-//         <img class="card-img-bottom" src="https://mdbootstrap.com/img/Photos/Horizontal/Work/4-col/img%20%2821%29.jpg" alt="Card image cap">
-//         <a>
-//             <div class="mask rgba-white-slight"></div>
-//         </a>
-//     </div>                            
-// </div> -->
-// <!-- <hr> -->
---
+++ assets/components/recommendationVideos.js
@@ -0,0 +1,48 @@
+Vue.component('recommendation-videos', {
+    props: ['recom-vids'],
+    template: `
+    <div id="random-video" class="col-6 col-md-4 scroll">
+    <hr>
+        <div class="card offset-md-1" style="width:80%">
+            <div class="view overlay">
+                <img class="card-img-top" src="https://mdbootstrap.com/img/Photos/Horizontal/Nature/4-col/img%20%28131%29.jpg" alt="Card image cap">
+                <a>
+                    <div class="mask rgba-white-slight"></div>
+                </a>
+            </div>
+            <div class="card-body elegant-color white-text">
+                <a href="#!" class="d-flex justify-content-end"><h5>Watch Video <i class="fas fa-angle-double-right"></i></h5></a>
+            </div>
+        </div>
+        <hr>
+    </div>
+    `
+})
+
+/* <div class="card offset-md-1" style="width:80%">
+<div class="view overlay">
+    <img class="card-img-top" src="https://mdbootstrap.com/img/Photos/Horizontal/Nature/4-col/img%20%28131%29.jpg" alt="Card image cap">
+    <a>
+        <div class="mask rgba-white-slight"></div>
+    </a>
+</div>
+
+<div class="card-body elegant-color white-text">
+    <a href="#!" class="d-flex justify-content-end"><h5>Watch Video <i class="fas fa-angle-double-right"></i></h5></a>
+</div>
+</div>
+<hr>
+
+<div class="card offset-md-1" style="width:80%">
+<div class="view overlay">
+    <img class="card-img-top" src="https://mdbootstrap.com/img/Photos/Horizontal/Nature/4-col/img%20%28131%29.jpg" alt="Card image cap">
+    <a>
+        <div class="mask rgba-white-slight"></div>
+    </a>
+</div>
+
+<div class="card-body elegant-color white-text">
+    <a href="#!" class="d-flex justify-content-end"><h5>Watch Video <i class="fas fa-angle-double-right"></i></h5></a>
+</div>
+</div>
+<hr> */
